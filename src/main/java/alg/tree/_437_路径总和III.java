c80a0f770415ac8438f39134df221d492e54ce56
fan1607 <yuefanlee@gmail.com>
2020-04-16 18:42:58 +0800

    437.路径总和III（优化）

--- src/main/java/alg/tree/_437_路径总和III.java
+++ src/main/java/alg/tree/_437_路径总和III.java
@@ -1,10 +1,5 @@
 package alg.tree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
 public class _437_路径总和III {
 
 //    public int pathSum(TreeNode root, int sum) {
@@ -43,44 +38,78 @@ public class _437_路径总和III {
 //        map.put(node, res);
 //    }
 
+//    private int count = 0;
+//
+//    public int pathSum(TreeNode root, int sum) {
+//        HashMap<TreeNode, List<Integer>> map = new HashMap<>();
+//        cal(root, sum, map);
+//        return count;
+//    }
+//
+//    private void cal(final TreeNode node, final int sum, final HashMap<TreeNode, List<Integer>> map) {
+//        if (node == null)
+//            return;
+//        List<Integer> res = new LinkedList<>();
+//        res.add(node.val);
+//        if (node.val == sum)
+//            ++count;
+//        cal(node.left, sum, map);
+//        cal(node.right, sum, map);
+//        List<Integer> childRes;
+//        int s;
+//        if (node.left != null) {
+//            childRes = map.remove(node.left);
+//            for (Integer val : childRes) {
+//                s = node.val + val;
+//                res.add(s);
+//                if (s == sum)
+//                    ++count;
+//            }
+//        }
+//        if (node.right != null) {
+//            childRes = map.remove(node.right);
+//            for (Integer val : childRes) {
+//                s = node.val + val;
+//                res.add(s);
+//                if (s == sum)
+//                    ++count;
+//            }
+//        }
+//        map.put(node, res);
+//    }
+
     private int count = 0;
 
     public int pathSum(TreeNode root, int sum) {
-        HashMap<TreeNode, List<Integer>> map = new HashMap<>();
-        cal(root, sum, map);
+        cal(root, sum);
         return count;
     }
 
-    private void cal(final TreeNode node, final int sum, final HashMap<TreeNode, List<Integer>> map) {
+    private int[] cal(final TreeNode node, final int sum) {
         if (node == null)
-            return;
-        List<Integer> res = new LinkedList<>();
-        res.add(node.val);
+            return null;
+        int[] lc = cal(node.left, sum);
+        int[] rc = cal(node.right, sum);
         if (node.val == sum)
             ++count;
-        cal(node.left, sum, map);
-        cal(node.right, sum, map);
-        List<Integer> childRes;
-        int s;
-        if (node.left != null) {
-            childRes = map.remove(node.left);
-            for (Integer val : childRes) {
-                s = node.val + val;
-                res.add(s);
-                if (s == sum)
+        int[] res = new int[1 + (lc == null ? 0 : lc.length) + (rc == null ? 0 : rc.length)];
+        res[0] = node.val;
+        int i = 1;
+        if (lc != null) {
+            for (int val : lc) {
+                res[i] = node.val + val;
+                if (res[i++] == sum)
                     ++count;
             }
         }
-        if (node.right != null) {
-            childRes = map.remove(node.right);
-            for (Integer val : childRes) {
-                s = node.val + val;
-                res.add(s);
-                if (s == sum)
+        if (rc != null) {
+            for (int val : rc) {
+                res[i] = node.val + val;
+                if (res[i++] == sum)
                     ++count;
             }
         }
-        map.put(node, res);
+        return res;
     }
 
     // TODO 看题解提到前缀和
