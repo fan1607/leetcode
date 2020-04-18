4d5afc5a01c266fd7574d76db1703482e271fc8e
fan1607 <yuefanlee@gmail.com>
2020-04-18 14:39:44 +0800

    _617_合并二叉树（TODO）

---
+++ src/main/java/alg/tree/_617_合并二叉树.java
@@ -0,0 +1,18 @@
+package alg.tree;
+
+/**
+ * TODO 一时没想出来，看的题解
+ */
+public class _617_合并二叉树 {
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
