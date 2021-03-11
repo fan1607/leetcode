6c7e33a41c34aba9ca97c4c2b47d63ad2c66acd4
fan1607 <yuefanlee@gmail.com>
2021-03-11 14:24:37 +0800

    二叉搜索树的最近公共祖先

--- src/main/java/alg/tree/_235_二叉搜索树的最近公共祖先.java
+++ src/main/java/alg/tree/_235_二叉搜索树的最近公共祖先.java
@@ -1,10 +1,9 @@
 package alg.tree;
 
-import java.util.HashSet;
-
 public class _235_二叉搜索树的最近公共祖先 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // TODO key -> 二叉搜索树的性质
         if (root.val > p.val && root.val > q.val)
             return lowestCommonAncestor(root.left, p, q);
         if (root.val < p.val && root.val < q.val)
@@ -12,30 +11,30 @@ public class _235_二叉搜索树的最近公共祖先 {
         return root;
     }
 
-    private HashSet<TreeNode> ancestors = new HashSet<>();
-    private TreeNode lowestAncestor = null;
-
-    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-        findPosition(root, p, false);
-        findPosition(root, q, true);
-        return lowestAncestor;
-    }
-
-    private void findPosition(TreeNode root, TreeNode target, boolean match) {
-        if (root == null)
-            return;
-        if (match) {
-            if (ancestors.contains(root)) {
-                lowestAncestor = root;
-            } else {
-                return;
-            }
-        } else {
-            ancestors.add(root);
-        }
-        if (root.val > target.val)
-            findPosition(root.left, target, match);
-        else if (root.val < target.val)
-            findPosition(root.right, target, match);
-    }
+//    private HashSet<TreeNode> ancestors = new HashSet<>();
+//    private TreeNode lowestAncestor = null;
+//
+//    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+//        findPosition(root, p, false);
+//        findPosition(root, q, true);
+//        return lowestAncestor;
+//    }
+//
+//    private void findPosition(TreeNode root, TreeNode target, boolean match) {
+//        if (root == null)
+//            return;
+//        if (match) {
+//            if (ancestors.contains(root)) {
+//                lowestAncestor = root;
+//            } else {
+//                return;
+//            }
+//        } else {
+//            ancestors.add(root);
+//        }
+//        if (root.val > target.val)
+//            findPosition(root.left, target, match);
+//        else if (root.val < target.val)
+//            findPosition(root.right, target, match);
+//    }
 }
---
+++ src/main/java/alg/tree/_剑指_Offer_68_I_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,14 @@
+package alg.tree;
+
+public class _剑指_Offer_68_I_二叉搜索树的最近公共祖先 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
