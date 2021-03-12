3f51472a34c6b6ae602ab3ccdb4e154d25398165
fan1607 <yuefanlee@foxmail.com>
2021-03-12 10:56:10 +0800

    update

--- src/main/java/alg/tree/_剑指Offer_68_I_二叉搜索树的最近公共祖先.java
+++ src/main/java/alg/tree/_剑指Offer_68_I_二叉搜索树的最近公共祖先.java
@@ -1,6 +1,6 @@
 package alg.tree;
 
-public class _剑指_Offer_68_I_二叉搜索树的最近公共祖先 {
+public class _剑指Offer_68_I_二叉搜索树的最近公共祖先 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root.val > p.val && root.val > q.val) {
