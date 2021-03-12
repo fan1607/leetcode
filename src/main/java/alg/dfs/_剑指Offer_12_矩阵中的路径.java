6900751d0ee87c73c30b6299e7ec4d6c167f8414
fan1607 <yuefanlee@foxmail.com>
2021-03-12 10:09:18 +0800

    剑指Offer_12_矩阵中的路径（DFS）

---
+++ src/main/java/alg/dfs/_剑指Offer_12_矩阵中的路径.java
@@ -0,0 +1,34 @@
+package alg.dfs;
+
+public class _剑指Offer_12_矩阵中的路径 {
+    public boolean exist(char[][] board, String word) {
+        char[] targets = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, targets, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, char[] targets, int i, int j, int k) {
+        // TODO 剪枝
+        if (i >= board.length || i < 0
+                || j >= board[0].length || j < 0
+                || board[i][j] != targets[k])
+            return false;
+        if (k == targets.length - 1)
+            return true;
+        // TODO 标记搜索记录
+        board[i][j] = '\0';
+        // TODO 上下左右前进搜索
+        boolean res = dfs(board, targets, i + 1, j, k + 1)
+                || dfs(board, targets, i - 1, j, k + 1)
+                || dfs(board, targets, i, j + 1, k + 1)
+                || dfs(board, targets, i, j - 1, k + 1);
+        // TODO 还原搜索记录
+        board[i][j] = targets[k];
+        return res;
+    }
+}
